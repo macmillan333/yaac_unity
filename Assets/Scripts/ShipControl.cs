eb95561c686657436a37e33de9f60def2340915f
macmillan333 <tengyuechina@outlook.com>
2019-08-01 20:04:47 -0700

    Player movement. Also improved warp logic.

---
+++ Assets/Scripts/ShipControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipControl : MonoBehaviour
+{
+    public float thrust;
+    public float torque;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        GetComponent<Rigidbody>().AddTorque(0f, horizontal * torque, 0f);
+
+        float angleInRadian = transform.rotation.eulerAngles.y * Mathf.Deg2Rad;
+        // No idea why x needs to be negated. It's a hackathon. Who cares.
+        Vector3 facingDirection = new Vector3(-Mathf.Cos(angleInRadian), 0f, Mathf.Sin(angleInRadian));
+        float vertical = 0f;
+        if (Input.GetButton("Thrust")) vertical = 1f;
+        if (Input.GetButton("Brake")) vertical = -1f;
+        GetComponent<Rigidbody>().AddForce(facingDirection * vertical * thrust);
+    }
+}
--- Assets/Scripts/Warp.cs
+++ Assets/Scripts/Warp.cs
@@ -2,31 +2,63 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Makes the GameObject warp around the WarpBorder. I have tried an approach based on OnTriggerExit
+// but it's not reliable on player's ship.
 public class Warp : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Longest half-side of bounding box. Used to detect when object is outside warp border.
+    private float extent;
+    private Rigidbody body;
+
+    private void Start()
     {
-        GameObject.Find("Sphere").GetComponent<Rigidbody>().velocity = new Vector3(1.920f, 0f, 1.08f);
+        extent = 0f;
+        Collider collider = GetComponent<Collider>();
+        body = GetComponent<Rigidbody>();
+        if (collider == null) return;
+        extent = collider.bounds.extents.x;
+        if (collider.bounds.extents.y > extent) extent = collider.bounds.extents.y;
+        if (collider.bounds.extents.z > extent) extent = collider.bounds.extents.z;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.layer != LayerMask.NameToLayer("WarpBorder")) return;
-        Vector3 exitPoint = other.ClosestPoint(transform.position);
-        if (exitPoint.x >= WarpBorder.borderSize.x || exitPoint.x <= -WarpBorder.borderSize.x)
+        // Condition to warp:
+        // - Object is >= `extent` units away from warp border
+        // - Object has velocity pointing away from warp border
+        if (transform.position.x > WarpBorder.borderSize.x + extent
+            && body.velocity.x > 0f)
         {
             transform.position = new Vector3(-transform.position.x, 0f, transform.position.z);
         }
-        if (exitPoint.z >= WarpBorder.borderSize.z || exitPoint.z <= -WarpBorder.borderSize.z)
+        if (transform.position.x < -WarpBorder.borderSize.x - extent
+            && body.velocity.x < 0f)
+        {
+            transform.position = new Vector3(-transform.position.x, 0f, transform.position.z);
+        }
+        if (transform.position.z > WarpBorder.borderSize.z + extent
+            && body.velocity.z > 0f)
+        {
+            transform.position = new Vector3(transform.position.x, 0f, -transform.position.z);
+        }
+        if (transform.position.z < -WarpBorder.borderSize.z - extent
+            && body.velocity.z < 0f)
         {
             transform.position = new Vector3(transform.position.x, 0f, -transform.position.z);
         }
     }
+
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    if (other.gameObject.layer != LayerMask.NameToLayer("WarpBorder")) return;
+    //    Vector3 exitPoint = other.ClosestPoint(transform.position);
+    //    if (Mathf.Abs(exitPoint.x) >= WarpBorder.borderSize.x - 0.1f)
+    //    {
+    //        transform.position = new Vector3(-transform.position.x, 0f, transform.position.z);
+    //    }
+    //    if (Mathf.Abs(exitPoint.z) >= WarpBorder.borderSize.z - 0.1f)
+    //    {
+    //        transform.position = new Vector3(transform.position.x, 0f, -transform.position.z);
+    //    }
+    //}
 }
