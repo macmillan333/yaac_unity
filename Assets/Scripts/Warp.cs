d64d2aa43b11ab57d69b7ad41bed843a4ebc3eed
macmillan333 <tengyuechina@outlook.com>
2019-08-01 18:03:22 -0700

    Defined warp border. Stuff can now warp around it.

---
+++ Assets/Scripts/Warp.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.Find("Sphere").GetComponent<Rigidbody>().velocity = new Vector3(1.920f, 0f, 1.08f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer != LayerMask.NameToLayer("WarpBorder")) return;
+        Vector3 exitPoint = other.ClosestPoint(transform.position);
+        if (exitPoint.x >= WarpBorder.borderSize.x || exitPoint.x <= -WarpBorder.borderSize.x)
+        {
+            transform.position = new Vector3(-transform.position.x, 0f, transform.position.z);
+        }
+        if (exitPoint.z >= WarpBorder.borderSize.z || exitPoint.z <= -WarpBorder.borderSize.z)
+        {
+            transform.position = new Vector3(transform.position.x, 0f, -transform.position.z);
+        }
+    }
+}
---
+++ Assets/Scripts/WarpBorder.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarpBorder : MonoBehaviour
+{
+    public static Vector3 borderSize;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        borderSize = new Vector3(
+            Camera.main.orthographicSize * Screen.width / Screen.height,
+            1f,
+            Camera.main.orthographicSize
+            );
+        transform.localScale = borderSize * 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
