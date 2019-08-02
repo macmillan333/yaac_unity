8288ae248fa8a661e62487f5336d5bbe9a7fc260
macmillan333 <tengyuechina@outlook.com>
2019-08-01 20:53:11 -0700

    Defined asteroid properties and started asteroid spawner. It doesn't work yet.

---
+++ Assets/Scripts/AsteroidSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class AsteroidProperties
+{
+    [Tooltip("Takes this many bullets to destroy.")]
+    public int maxHp;
+    [Tooltip("Diameter.")]
+    public float size;
+    [Tooltip("When spawned, the asteroid is given a random velocity that does not exceed this amount.")]
+    public float maxSpeed;
+    [Tooltip("When destroyed, spawns this many next-level Asteroids."
+        + " The final level asteroid does not split further, and thus ignores this number.")]
+    public int numSplits;
+}
+
+public class AsteroidSpawner : MonoBehaviour
+{
+    public GameObject asteroidPrefab;
+    public List<AsteroidProperties> asteroidProperties;
+    public int numInitialAsteroids;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Spawn `numInitialAsteroids` top-level asteroids. Their positions should be carefully chosen so that:
+        // - they don't collide with each other
+        // - they are moderately far away from player ship
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
