f41eed2b576a7f5112b244933c635308a7f1e564
macmillan333 <tengyuechina@outlook.com>
2019-08-02 19:15:24 -0700

    Asteroids can now be destroyed. They may also spawn smaller asteroids when destroyed.

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    private List<AsteroidProperties> allProperties;
+    // Index into |properties|.
+    private int level;
+
+    private int maxHp;
+    private int hp;
+
+    public void SetProperties(List<AsteroidProperties> properties, int level)
+    {
+        this.allProperties = properties;
+        this.level = level;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AsteroidProperties properties = allProperties[level];
+        maxHp = properties.maxHp;
+        hp = maxHp;
+
+        float speed = Random.value * properties.maxSpeed;
+        float angle = Random.value * Mathf.PI * 2f;
+        GetComponent<Rigidbody>().velocity = new Vector3(
+            Mathf.Cos(angle), 0f, Mathf.Sin(angle)) * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Bullet"))
+        {
+            hp--;
+            Destroy(other.gameObject);
+            if (hp <= 0)
+            {
+                if (level < allProperties.Count - 1)
+                {
+                    SpawnNextLevelAsteroids();
+                }
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    private void SpawnNextLevelAsteroids()
+    {
+        AsteroidProperties thisProperties = allProperties[level];
+        Vector3 min = transform.position - transform.localScale * 0.5f;
+        Vector3 max = transform.position + transform.localScale * 0.5f;
+        int number = thisProperties.numSplits;
+        float nextDiameter = allProperties[level + 1].size;
+
+        foreach (Vector3 l in AsteroidSpawner.FindSpawnLocations(min, max, number,
+            nextDiameter, 0f))
+        {
+            GameObject asteroid = Instantiate(gameObject);
+            asteroid.transform.position = l;
+            asteroid.transform.localScale = new Vector3(nextDiameter, nextDiameter, nextDiameter);
+            asteroid.GetComponent<Asteroid>().SetProperties(allProperties, level + 1);
+        }
+    }
+}
--- Assets/Scripts/AsteroidSpawner.cs
+++ Assets/Scripts/AsteroidSpawner.cs
@@ -22,58 +22,67 @@ public class AsteroidSpawner : MonoBehaviour
     public List<AsteroidProperties> asteroidProperties;
     public int numInitialAsteroids;
 
+    // Randomly chooses `number` vectors inside the rectangle min-max. All locations are guaranteed to be:
+    // - at least `minDistanceFromEachOther` units away from each other
+    // - at least `minDistanceFromOrigin` units away from (0, 0, 0)
+    // Will make 100 attempts on each location. If all attemps fail, throws InvalidOperationException.
+    public static List<Vector3> FindSpawnLocations(Vector3 min, Vector3 max, int number,
+        float minDistanceFromEachOther, float minDistanceFromOrigin)
+    {
+        List<Vector3> locations = new List<Vector3>();
+        for (int i = 0; i < number; i++)
+        {
+            Vector3 l = Vector3.zero;
+            int attempt = 0;
+            const int maxAttempt = 100;
+            for (; attempt < maxAttempt; attempt++)
+            {
+                l = new Vector3(
+                    Mathf.Lerp(min.x, max.x, Random.value),
+                    0f,
+                    Mathf.Lerp(min.z, max.z, Random.value));
+                if (l.magnitude < minDistanceFromOrigin) continue;
+
+                bool awayFromAllOthers = true;
+                foreach (Vector3 other in locations)
+                {
+                    if (Vector3.Distance(l, other) < minDistanceFromEachOther)
+                    {
+                        awayFromAllOthers = false;
+                        break;
+                    }
+                }
+                if (!awayFromAllOthers) continue;
+
+                // Proper location found.
+                break;
+            }
+            if (attempt == maxAttempt)
+            {
+                throw new System.InvalidOperationException("Failed to find spawn location after " + maxAttempt + " attempts.");
+            }
+
+            locations.Add(l);
+        }
+        return locations;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         // Spawn `numInitialAsteroids` top-level asteroids. Their positions should be carefully chosen so that:
         // - they don't collide with each other
         // - they areat least 5 units away from player ship
-        List<Vector3> spawnedLocations = new List<Vector3>();
         float diameter = asteroidProperties[0].size;
-        for (int i = 0; i < numInitialAsteroids; i++)
+        List<Vector3> spawnedLocations = FindSpawnLocations(-WarpBorder.borderSize, WarpBorder.borderSize,
+            numInitialAsteroids, diameter, diameter * 0.5f + 5f);
+
+        foreach (Vector3 l in spawnedLocations)
         {
-            Vector3 spawnLocation = Vector3.zero;
-            int attempt = 0;
-            for (; attempt < 100; attempt++)
-            {
-                spawnLocation = new Vector3(
-                    (Random.value - 0.5f) * WarpBorder.borderSize.x * 2f,
-                    0f,
-                    (Random.value - 0.5f) * WarpBorder.borderSize.z * 2f);
-                float distanceToOrigin = spawnLocation.magnitude;
-                if (distanceToOrigin <= diameter * 0.5f + 5f) continue;
-                bool awayFromAllOther = true;
-                foreach (Vector3 other in spawnedLocations)
-                {
-                    float distanceToOther = Vector3.Distance(other, spawnLocation);
-                    if (distanceToOther < diameter)
-                    {
-                        awayFromAllOther = false;
-                        break;
-                    }
-                }
-                if (!awayFromAllOther) continue;
-                break;
-            }
-            if (attempt == 100)
-            {
-                throw new System.InvalidOperationException("Failed to spawn asteroid after 100 attempts.");
-            }
-
             GameObject asteroid = Instantiate(asteroidPrefab);
-            asteroid.transform.position = spawnLocation;
+            asteroid.transform.position = l;
             asteroid.transform.localScale = new Vector3(diameter, diameter, diameter);
-            float speed = Random.value * asteroidProperties[0].maxSpeed;
-            float angle = Random.value * Mathf.PI * 2f;
-            asteroid.GetComponent<Rigidbody>().velocity = new Vector3(
-                Mathf.Cos(angle), 0f, Mathf.Sin(angle)) * speed;
-            spawnedLocations.Add(spawnLocation);
+            asteroid.GetComponent<Asteroid>().SetProperties(asteroidProperties, 0);
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
