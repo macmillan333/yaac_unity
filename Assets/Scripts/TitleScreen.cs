6e4635ea9d67ed61c462bd9e3730fdd1444991b8
macmillan333 <tengyuechina@outlook.com>
2019-08-11 12:44:39 -0700

    Title screen, part 1: animation and "Press S+D+F+J+K+L to start".

---
+++ Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScreen : MonoBehaviour
+{
+    public Animator animator;
+    public AudioSource startSound;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("TitleScreenWaitForStart"))
+        {
+            // Waiting for start
+            if (Input.GetKey(KeyCode.S) &&
+                Input.GetKey(KeyCode.D) &&
+                Input.GetKey(KeyCode.F) &&
+                Input.GetKey(KeyCode.J) &&
+                Input.GetKey(KeyCode.K) &&
+                Input.GetKey(KeyCode.L))
+            {
+                animator.SetTrigger("PressedStart");
+                startSound.Play();
+            }
+        }
+    }
+}
